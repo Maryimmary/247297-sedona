51e4d6f267d2e5de55854625b4e35cd62dfca826
Maryimmary <maryimmary@yandex.ru>
2017-05-07 16:10:35 +0300

    исправления

--- js/script.js
+++ js/script.js
@@ -6,30 +6,31 @@ var adultCount = popup.querySelector("#adult-count");
 var childCount = popup.querySelector("#child-count");
 var storage = localStorage.getItem("adultCount");
 
+popup.classList.remove("search-menu-show");
+
 link.addEventListener("click", function(event) {
 event.preventDefault();
-if ( popup.classList.contains("search-menu-show") ) {
+if (popup.classList.contains("search-menu-show")) {
   popup.classList.remove("search-menu-show");
 }
 else {
- popup.classList.add("search-menu-show");
-   if (storage) {
-     adultCount.value = storage;
-     childCount.focus();
-   }
-   else {
-     adultCount.focus();
-   }
+  popup.classList.add("search-menu-show");
+  if (storage) {
+    adultCount.value = storage;
+    childCount.focus();
+  }
+  else {
+    adultCount.focus();
+  }
 }
 });
 
 form.addEventListener("submit", function(event) {
-
- if (!adultCount.value || !childCount.value) {
-   event.preventDefault();
-   console.log("Нужно заполнить поля информации");
- }
- else {
-   localStorage.setItem("adultCount", adultCount.value);
- }
+if (!adultCount.value || !childCount.value) {
+  event.preventDefault();
+  console.log("Нужно заполнить поля информации");
+}
+else {
+  localStorage.setItem("adultCount", adultCount.value);
+}
 });
